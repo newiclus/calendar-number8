9e6fbc24059a5ff78b23d26802e9e046de12253e
Junihor Moran <junihor.moran@wunderman.com>
2018-01-17 14:05:30 -0500

    Add styles and functionality of days

--- dev-assets/js/main.js
+++ dev-assets/js/main.js
@@ -5,14 +5,66 @@ const APP = {
   home: function () {
     
     const ele = {
-      startDay: $("#start_day"),
+      startDay: $("#start-day"),
+      numberDays: $("#number-days"),
+      dateInput: $("#date-input")
     };
 
-    // Interactive demo
-	  var datePicker = ele.startDay.glDatePicker({
-      showAlways: true
+    // Init default date
+    ele.dateInput.val(ele.startDay.val());
+
+
+    var getRangeDays = function (numberDays) {
+      var rangeDays = [];
+      var startDate = new Date( ele.startDay.val() );
+
+      for (var i = 0; i < numberDays; i++) {
+        var dateRange = startDate.setDate(startDate.getDate() + 1);
+        rangeDays.push({
+          date: new Date(dateRange),
+          data: { message: 'Selected by range' },
+          repeatMonth: false
+        });
+      }
+      
+      return rangeDays;
+    };
+
+    // Instance glDatePicker
+	  var datePicker = ele.dateInput.glDatePicker({
+      showAlways: true,
+      cssName: 'flatwhite',
+      // Init default date
+      todayDate: new Date( ele.startDay.val() ),
+    
+      specialDates: getRangeDays(ele.numberDays.val()),
+      
+      onClick: function(target, cell, date, data) {
+        target.val(
+          date.getFullYear() + ' - ' +
+          date.getMonth() + ' - ' +
+          date.getDate()
+        );
+
+        if(data != null) {
+          alert(data.message + '\n' + date);
+        }
+      }
     }).glDatePicker(true);
 
+    // Update datePicker
+    ele.startDay.on('input', function() {
+      if (ele.startDay.val() !== '') {
+        datePicker.options.todayDate = new Date( ele.startDay.val() );
+        datePicker.render();
+      }
+      
+    });
+
+    ele.numberDays.on('input', function() {
+      datePicker.options.specialDates = getRangeDays(ele.numberDays.val());
+      datePicker.render();
+    });
 
   }
 };
